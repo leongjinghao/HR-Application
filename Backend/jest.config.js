ef832a30b284f0dc69e66ee861071cc306fff3ff
Caden <2002918@sit.singaporetech.edu.sg>
2022-02-22 12:40:42 +0800

    update

--- Backend/jest.config.js
+++ Backend/jest.config.js
@@ -2,4 +2,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  coverageDirectory: './tests/coverage',
+  verbose: true,
+  collectCoverage: true,
+  coverageReporters: ['html', 'text', 'text-summary'],
 };
