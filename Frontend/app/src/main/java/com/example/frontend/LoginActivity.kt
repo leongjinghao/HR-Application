3a0b4fba0323d53556f9339b34d4f5facb236105
wjayjay123 <77979635+wjayjay123@users.noreply.github.com>
2022-02-25 15:58:47 +0800

    Fingerprint Added

--- Frontend/app/src/main/java/com/example/frontend/LoginActivity.kt
+++ Frontend/app/src/main/java/com/example/frontend/LoginActivity.kt
@@ -1,19 +1,85 @@
 package com.example.frontend
 
+import android.app.KeyguardManager
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.hardware.biometrics.BiometricPrompt
+import android.os.Build
 import android.os.Bundle
+import android.os.CancellationSignal
 import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 
 class LoginActivity : AppCompatActivity()  {
 
     val TAG : String = "Login"
 
+    private var cancellationSignal: CancellationSignal? = null
+
+    //Handles fingerprint authentication
+    private var authenticationCallback: BiometricPrompt.AuthenticationCallback
+        get() =
+            @RequiresApi(Build.VERSION_CODES.P)
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
+                    super.onAuthenticationError(errorCode, errString)
+                    authMsg("Authentication error: $errString")
+                }
+
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult?) {
+                    super.onAuthenticationSucceeded(result)
+                    authMsg("Authentication successful")
+                    startActivity(Intent(this@LoginActivity, HomePageMainActivity::class.java))
+                }
+            }
+        set(value) {}
+
+    //Check if user cancel the fingerprint authentication
+    private fun getCancellationSignal():CancellationSignal{
+        cancellationSignal = CancellationSignal()
+        cancellationSignal?.setOnCancelListener {
+            authMsg("Authentication cancelled")
+        }
+        return cancellationSignal as CancellationSignal
+    }
+
+    private fun checkBiometricSupport():Boolean{
+        //Check if smartphone supports fingerprint
+        val keyguardManager:KeyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+        if(!keyguardManager.isKeyguardSecure){
+            authMsg("Fingerprint authentication not enabled")
+            return false
+        }
+        // Check if fingerprint permisson is enabled
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.USE_BIOMETRIC) != PackageManager.PERMISSION_GRANTED){
+            authMsg("Fingerprint authentication permission is not enabled")
+            return false
+        }
+        return if(packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT)){
+            return true
+        }
+        else true
+    }
+
+    //Create toast with message
+    private fun authMsg(message:String){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        checkBiometricSupport()
 
         // findViewById for UI elements
         val usernameEditText = findViewById<EditText>(R.id.editTextUsernameLogin)
@@ -32,10 +98,23 @@ class LoginActivity : AppCompatActivity()  {
             // TODO create intent to Home page
         }
 
+        //Note: Have to manually add a fingerprint to the emulator
+        //To emulate fingerprint
+        //Open CMD at Appdata->Local->Android->platformtools
+        //Command: adb -e emu finger touch <fingerprint id>
+        //<fingerprint id> can be any value
+        //Example usuage adb -e emu finger touch 123abc
         fingerprintLoginButton.setOnClickListener {
-            // TODO implement authentication logic
+            //Create Fingerprint drawer on button click
+            val biometricPrompt = BiometricPrompt.Builder(this)
+                .setTitle("Fingerprint Login")
+                .setSubtitle("Fingerprint Authentication is required")
+                .setDescription("Please scan your fingerprint below")
+                .setNegativeButton("Cancel", this.mainExecutor,DialogInterface.OnClickListener{dialog, which ->
+                    authMsg("Authentication cancelled")
+                }).build()
 
-            // TODO create intent to Home page
+            biometricPrompt.authenticate(getCancellationSignal(),mainExecutor, authenticationCallback)
         }
 
     }
