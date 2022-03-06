42844c482ab739c3d441562ffcb237a98c41b5be
leongjinghao <73938217+leongjinghao@users.noreply.github.com>
2022-03-07 02:28:02 +0800

    Create HistoryRoomDatabase.kt

---
+++ Frontend/app/src/main/java/com/example/frontend/HistoryRoomDatabase.kt
@@ -0,0 +1,57 @@
+package com.example.frontend
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@Database(entities = [History::class], version = 1, exportSchema = false)
+abstract class HistoryRoomDatabase : RoomDatabase() {
+
+    abstract fun HistoryDao(): HistoryDao
+
+    companion object {
+        // Singleton prevents multiple instances of database opening at the same time
+        @Volatile
+        private var INSTANCE: HistoryRoomDatabase? = null
+
+        fun getDatabase(context: Context, scope: CoroutineScope): HistoryRoomDatabase {
+            // if INSTANCE is not null, return it
+            // else, create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    HistoryRoomDatabase::class.java,
+                    "history_database"
+                )
+                    .addCallback(HistoryDatabaseCallback(scope))
+                    .build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
+
+    private class HistoryDatabaseCallback(
+        private val scope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    populateDatabase(database.HistoryDao())
+                }
+            }
+        }
+
+        suspend fun populateDatabase(historyDao: HistoryDao) {
+            // Delete all content
+            historyDao.deleteAll()
+        }
+    }
+}
