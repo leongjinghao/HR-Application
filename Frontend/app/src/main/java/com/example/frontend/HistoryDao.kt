481a7065fe0933bdf443456da9635f5d659677f9
leongjinghao <73938217+leongjinghao@users.noreply.github.com>
2022-03-07 02:27:30 +0800

    Create HistoryDao.kt

---
+++ Frontend/app/src/main/java/com/example/frontend/HistoryDao.kt
@@ -0,0 +1,22 @@
+package com.example.frontend
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface HistoryDao {
+    // Retrieve all records of check in and out history
+    @Query("SELECT * FROM HistoryTable")
+    fun getAllHistory(): Flow<List<History>>
+
+    // Insert new check in and out record into table
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(history: History)
+
+    // Delete all check in and out history from table
+    @Query("DELETE FROM HistoryTable")
+    suspend fun deleteAll()
+}
