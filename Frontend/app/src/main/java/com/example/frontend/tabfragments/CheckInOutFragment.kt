5c5b7700b28d59011936fef30106b7117ece160c
leongjinghao <73938217+leongjinghao@users.noreply.github.com>
2022-03-30 02:28:18 +0800

    Updated logic to insert record on check out
    Inserted record are only done to room DB in this commit

--- Frontend/app/src/main/java/com/example/frontend/tabfragments/CheckInOutFragment.kt
+++ Frontend/app/src/main/java/com/example/frontend/tabfragments/CheckInOutFragment.kt
@@ -1,17 +1,28 @@
 package com.example.frontend.tabfragments
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import com.example.frontend.CheckInDetailActivity
-import com.example.frontend.R
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.viewModels
+import com.example.frontend.*
 import com.example.frontend.leaveModule.LeaveSummaryActivity
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class CheckInOutFragment : Fragment() {
+
+    // create the ViewModel
+    private val historyViewModel: HistoryViewModel by viewModels {
+        HistoryViewModelFactory((activity?.applicationContext as HRApplication).repository)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,10 +38,34 @@ class CheckInOutFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val checkinButton = view.findViewById<Button>(R.id.buttonCheckInOut)
-        checkinButton.setOnClickListener{
-            val intent = Intent (activity, CheckInDetailActivity::class.java)
-            activity?.startActivity(intent)
+        val checkInButton = view.findViewById<Button>(R.id.buttonCheckInOut)
+        val checkInButtonText = view.findViewById<TextView>(R.id.textViewCheckInOut)
+
+        // Configure button text according to the check in and out status
+        if (historyViewModel.checkInOutStatus == "Clock out") {
+            checkInButtonText.text = "Check In"
+        }
+        else {
+            checkInButtonText.text = "Check Out"
+        }
+
+        checkInButton.setOnClickListener{
+            // if check in and out status is "Clock out", display check in button/option
+            if (historyViewModel.checkInOutStatus == "Clock out") {
+                val intent = Intent(activity, CheckInDetailActivity::class.java)
+                activity?.startActivity(intent)
+            }
+            else {
+                // TODO: additional check out logic on aws db
+
+                // Insert check out record on room DB
+                historyViewModel.insert(History(
+                    LocalDate.now().toString(),
+                    LocalDate.now().dayOfWeek.name,
+                    "Clock out",
+                    LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm"))
+                ))
+            }
         }
     }
 }
