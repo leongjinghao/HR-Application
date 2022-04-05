b02f63d3c4b0f3026bcfd189da96d07fd9a0784c
Caden <2002918@sit.singaporetech.edu.sg>
2022-04-05 17:57:27 +0800

    sesSendEmail unit test

--- Backend/src/handlers/utility/__test__/sesSendEmail.unit.test.ts
+++ Backend/src/handlers/utility/__test__/sesSendEmail.unit.test.ts
@@ -1,7 +1,6 @@
 /* eslint-disable no-console */
 import { handler as sesSendEmail } from '../sesSendEmail'
 import { LambdaResponse } from '../responses'
-import AWSMock from 'aws-sdk-mock'
 import AWS from 'aws-sdk'
 
 console.log = jest.fn
@@ -9,22 +8,34 @@ console.error = jest.fn
 
 let matchResponse: LambdaResponse
 
+jest.mock('aws-sdk', () => {
+    const sesSendEmailPromiseMock = jest.fn()
+    .mockResolvedValue(true)
+    return {
+      SES: jest.fn(() => ({
+        sendEmail: () => ({
+          promise: sesSendEmailPromiseMock
+        }),
+      }),
+      )}
+  })
+
 beforeEach(() => {
     jest.clearAllMocks()
   })
 
+const ses = new AWS.SES({})
+
 describe('/utility/sesSendEmail sesSendEmail.ts test', () => {
     test('Send email successfully', async () => {
-        AWSMock.setSDKInstance(AWS)
-        AWSMock.mock('SES',sesSendEmail,)
+        (ses.sendEmail().promise as jest.Mock).mockResolvedValue(true)
         matchResponse = { statusCode: 200, body: 'Email Sent Successfully' }
         const response = await sesSendEmail(
             {body : JSON.stringify({
                 recipientEmail : 'test@gmail.com',
             })}
         )
-        expect(response.statusCode).toStrictEqual(matchResponse.statusCode)
-        expect(response.body).toStrictEqual(matchResponse.body)
+        expect(response.body).toStrictEqual(JSON.stringify({result:true,message:'Email Sent Successfully'}))
     })
 
   test('Fail to send email, event data as undefined', async () => {
